2257a9e50d516b5a1685b0193b1b1a16ab8a2ddc
4flagowner <142928834+4flagowner@users.noreply.github.com>
2023-08-25 23:53:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94726639722', 'janith_ofc', true],
+  ['94752013704', 'janith_ofc', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
